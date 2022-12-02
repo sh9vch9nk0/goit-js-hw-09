734b178c93c351cfaab37fe044a58c0387bffaad
sh9vch9nk0 <sh9vch9nk0@gmail.com>
2022-12-02 17:35:20 +0200

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,5 +1,6 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
+import Notiflix from 'notiflix';
 const timerEl = document.querySelector('.timer');
 const fieldElList = document.querySelectorAll('.field');
 const startBtn = document.querySelector('[data-start]');
